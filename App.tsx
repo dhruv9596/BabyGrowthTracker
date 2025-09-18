c65ad6e2fad0fbbdca700df0ae9cade89d31a35b
dhruv9596 <dhruvlodaliya9596@gmail.com>
2025-09-18 23:14:54 +0530

    Date Picker and Minor Updates

--- App.tsx
+++ App.tsx
@@ -1,31 +1,3 @@
-/*
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./HomeScreen";
-import HistoryScreen from "./HistoryScreen";
-import GrowthChartScreen from "./GrowthChartScreen";
-
-const Tab = createBottomTabNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: true,
-          tabBarActiveTintColor: "tomato",
-          tabBarInactiveTintColor: "gray",
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="History" component={HistoryScreen} />
-        <Tab.Screen name="Chart" component={GrowthChartScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
-*/
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -39,25 +11,25 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-  screenOptions={({ route }) => ({
-    headerShown: true,
-    tabBarIcon: ({ color, size }) => {
-      let iconName: string = "";
+        screenOptions={({ route }) => ({
+          headerShown: true,
+          tabBarIcon: ({ color, size }) => {
+            let iconName: string = "";
 
-      if (route.name === "Home") iconName = "home";
-      else if (route.name === "Chart") iconName = "bar-chart";
-      // if you add History screen back:
-      // else if (route.name === "History") iconName = "time";
+            if (route.name === "Home") iconName = "home";
+            else if (route.name === "Chart") iconName = "bar-chart";
+            // if you add History screen back:
+            // else if (route.name === "History") iconName = "time";
 
-      return <Ionicons name={iconName} size={size} color={color} />;
-    },
-    tabBarActiveTintColor: "tomato",
-    tabBarInactiveTintColor: "gray",
-  })}
->
-  <Tab.Screen name="Home" component={HomeScreen} />
-  <Tab.Screen name="Chart" component={GrowthChartScreen} />
-</Tab.Navigator>
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Chart" component={GrowthChartScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
